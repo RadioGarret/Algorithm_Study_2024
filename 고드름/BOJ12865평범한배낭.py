7c031f0c11f3d12450be6f008f02c88a5cf6b648
김효주 <hyojukim.dev@gmail.com>
2024-07-11 21:30:15 +0900

    BOJ2468 완전탐색 안전영역

--- 고드름/BOJ12865평범한배낭.py
+++ 고드름/BOJ12865평범한배낭.py
@@ -1,3 +1,19 @@
+N, K = list(map(int, input().split()))
+products = [list(map(int, input().split())) for _ in range(N)]
+
+# dp[i] 는 최대 i 무게에서 얻을 수 있는 최대 가치를 의미
+dp = [0] * (K+1)
+
+# 물건을 배낭에 넣기
+for weight, value in products:
+    for i in range(K, weight - 1, -1):
+        dp[i] = max(dp[i], dp[i - weight] + value)
+
+answer = dp[K]
+print(answer)
+
+
+'''
 # DFS 시간초과 -> DP 문제 ...
 
 # idx 번째 물건을 선택할 지 아닐 지 결정 하는 함수
@@ -29,3 +45,4 @@ elif K == min_w:
 else:
     dfs(0,0,0,[0]*N)
     print(max_v)
+'''
